0b10860f68f0f69775dcf7f465a4c8bb491403eb
fabriciodcm <fa.docema@gmail.com>
2020-04-14 23:43:46 -0300

    first commit

---
+++ ChemQuiz/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ChemQuiz.Services;
+using ChemQuiz.Views;
+
+namespace ChemQuiz
+{
+    public partial class App : Application
+    {
+        //TODO: Replace with *.azurewebsites.net url after deploying backend to Azure
+        //To debug on Android emulators run the web backend against .NET Core not IIS
+        //If using other emulators besides stock Google images you may need to adjust the IP address
+        public static string AzureBackendUrl =
+            DeviceInfo.Platform == DevicePlatform.Android ? "http://10.0.2.2:5000" : "http://localhost:5000";
+        public static bool UseMockDataStore = true;
+
+        public App()
+        {
+            InitializeComponent();
+            XF.Material.Forms.Material.Init(this);
+
+            if (UseMockDataStore)
+                DependencyService.Register<MockDataStore>();
+            else
+                DependencyService.Register<AzureDataStore>();
+            
+            #if DEBUG
+                HotReloader.Current.Run(this);
+            #endif
+            
+            MainPage = new LoginPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ ChemQuiz/Views/LoginPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using ChemQuiz.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ChemQuiz.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnEntrarButtonClicked(object sender, EventArgs e) {
+            App.Current.MainPage = new MainPage();
+            await (App.Current.MainPage as MasterDetailPage).Detail.Navigation.PushAsync(new ItemsPage());
+        }
+    }
+}
